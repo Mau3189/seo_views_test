990b46e9a8a91796c2b4be4354bd520bc806dac8
Mauricio Roldan Jimenez <mroldan@beenverified.com>
2018-05-03 11:11:26 -0600

    Add bv footer

---
+++ bv/parts/footer.php
@@ -0,0 +1,195 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-12 pull-right">
+        <div class="right-footer">
+          <h3 class="excellent">“Excellent Support & Service”</h3>
+          <img src="<?= ASSET_DIR; ?>/img/star.png" alt="star" class="pull-left" />
+          <span class="via">-via SiteJabber.com</span>
+        </div>
+        <div class="footer-contact">
+          <p>
+            <a href="tel:1-888-579-5910" class="foot-phone">1-888-579-5910</a>
+          </p>
+          <p>
+            <a href="mailto:support@beenverified.com" class="foot-email">support@beenverified.com </a>
+          </p>
+          <p>
+            <span class="foot-monday">Monday - Friday 8 am - 10 pm est
+            <br/> Saturday - Sunday 10 am - 8 pm est</span>
+          </p>
+        </div>
+        <div class="our-goal">
+          <p class="our-goal-para1">"Our goal is to be quick, friendly and as helpful as possible."</p>
+          <p class="our-goal-para2">-Dana, Customer Support</p>
+        </div>
+      </div>
+      <div class="col-md-8 col-sm-12">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="left-footer">
+              <h3 class="left-footer-header">About Us</h3>
+              <ul class="footer-list">
+                <li>
+                  <a href="https://www.beenverified.com/about/">About BV</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/about/testimonials/">Testimonials</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/careers/">Careers</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/about/leadership/">Leadership</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/press/">Press</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/people/">People White Pages</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="left-footer">
+              <h3 class="left-footer-header">BV Apps & Data</h3>
+              <ul class="footer-list">
+                <li>
+                  <a href="https://www.beenverified.com/apps/">All BV Apps</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/apps/ios/">For iPhone / iPad</a> - <a href="https://itunes.apple.com/us/app/background-check-app/id342585873?mt=8">Download</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/apps/apple-watch/">For Apple Watch</a> - <a href="https://itunes.apple.com/us/app/background-check-app-beenverified/id342585873?mt=8">Download</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/apps/android/">For Android</a> - <a href="https://market.android.com/details?id=com.beenverified.android&feature=search_result">Download</a>
+                </li>
+                <li>
+                  <a href="https://docs.google.com/a/beenverified.com/spreadsheet/viewform?formkey=dEU1elc5dDVoMElwYmlYUWQ0Wkxjbmc6MA">Developer API</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/bv-pro/">BV Pro</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="left-footer">
+              <h3 class="left-footer-header">Tools</h3>
+              <ul class="footer-list">
+                <li>
+                  <a href="https://www.beenverified.com/people/">People Search</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/reverse-phone/">Reverse Phone Lookup</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/email-search/">Email Lookup</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/reverse-address-lookup/">Reverse Address Lookup</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/public-records/">Public Records</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/criminal-records/">Criminal Records</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/arrest-records/">Arrest Records</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/people/">White Pages</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="left-footer">
+              <h3 class="left-footer-header">Help</h3>
+              <ul class="footer-list">
+                <li>
+                  <a href="http://support.beenverified.com/home?_ga=2.86961176.148677681.1523888234-1125970930.1520277921">Support</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/contact/">Contact Us</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/about/dos-donts/">Do's & Don'ts</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/faq/billing/">FAQ</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/faq/opt-out/">Remove My Info</a>
+                </li>
+                <li>
+                  <a href="https://www.beenverified.com/affiliates/">Affiliates</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <ul class="social">
+          <li>
+            <a class="social-fb" href="https://www.facebook.com/BeenVerified">
+              <span class="hidden-xs">Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a class="social-tw" href="https://twitter.com/beenverified">
+              <span class="hidden-xs">Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a class="social-go" href="https://plus.google.com/+beenverified/posts">
+              <span class="hidden-xs">Google+</span>
+            </a>
+          </li>
+          <li>
+            <a class="social-pin" href="https://www.pinterest.com/beenverified/">
+              <span class="hidden-xs">Pinterest</span>
+            </a>
+          </li>
+          <li>
+            <a class="social-lin" href="https://www.linkedin.com/company/beenverified-com">
+              <span class="hidden-xs">LinkedIn</span>
+            </a>
+          </li>
+          <li>
+            <a class="social-bv" href="https://www.beenverified.com/blog/">
+              <span class="hidden-xs">BV Blog</span>
+            </a>
+          </li>
+        </ul>
+        <div class="disclaimer">
+          Disclaimer: BeenVerified’s mission is to give people easy and affordable access to public record information. BeenVerified
+          does not provide private investigator services, and is not a consumer reporting agency as defined by the <a href="https://www.beenverified.com/about/fcra/">Fair Crediting
+          Report Act</a> because the information provided by BeenVerified is not collected or provided, in whole or in part, for
+          the purpose of furnishing consumer reports about those search subjects. For more information governing the permitted
+          and prohibited uses of BeenVerified, please review our <a href="https://www.beenverified.com/about/dos-donts/">“Do’s & Don’ts”</a> and our <a href="https://www.beenverified.com/faq/terms-conditions/">Terms & Conditions</a>.
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="copyright">
+           &copy; <?= date('Y'); ?> <span><strong>BeenVerified, Inc.</strong>,All Rights Reserved. BeenVerified©, and the BeenVerified star mascot are registered trademarks of BeenVerified, Inc.</span>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="terms">
+          <a href="https://www.beenverified.com/faq/terms-conditions/">Terms & Conditions </a> |
+          <a href="https://www.beenverified.com/faq/privacy/"> Privacy Policy</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
